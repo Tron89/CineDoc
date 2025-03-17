2a9f81b55e194fe38b6109f97cd4594c1a14b2b1
Claudiu <Claudiu@LENO-A.mshome.net>
2025-03-17 13:48:16 +0100

    pruebas en main

--- CineDoc/src/CinePK/mainCine.java
+++ CineDoc/src/CinePK/mainCine.java
@@ -2,7 +2,17 @@ package CinePK;
 
 public class mainCine {
 	public static void main(String[] args) {
-		Cine cine = new Cine(4, 10, "Terror en el instituto");
+		Cine cine = new Cine(3);
+		cine.venderEntrada();
+		cine.venderEntrada();
+		cine.venderEntrada();
+		cine.venderEntrada();
+		System.out.println(cine.getButacasOcupadas());
+		cine.devolverEntrada();
+		cine.devolverEntrada();
+		cine.devolverEntrada();
+		cine.devolverEntrada();
+		System.out.println(cine.getButacasOcupadas());
 	}
 }
  
